53fa136c1339e1717652053113f14ab32413b62c
Erik Svensson <eosvensson@gmail.com>
2022-02-18 09:20:31 +0100

    Made Measurement into entity

--- src/main/java/se/lexicon/recipeassignmentapi/model/Measurement.java
+++ src/main/java/se/lexicon/recipeassignmentapi/model/Measurement.java
@@ -1,12 +1,27 @@
 package se.lexicon.recipeassignmentapi.model;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "measurements")
 public class Measurement {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name ="UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(updatable = false, name = "id")
     private String id;
+    @Column(name = "measurement", unique = true)
     private String measurement;
 
     public Measurement(String id, String measurement) {
         this.id = id;
         this.measurement = measurement;
+    }
+
+    public Measurement() {
     }
 
     public String getId() {
