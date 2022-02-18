e9e374d10d248cedef3e7ad303cc6d949d3593fa
Erik Svensson <eosvensson@gmail.com>
2022-02-18 11:08:46 +0100

    Added some queries

--- src/main/java/se/lexicon/recipeassignmentapi/repository/CategoryRepository.java
+++ src/main/java/se/lexicon/recipeassignmentapi/repository/CategoryRepository.java
@@ -1,7 +1,13 @@
 package se.lexicon.recipeassignmentapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import se.lexicon.recipeassignmentapi.model.Category;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, String> {
+    @Query("SELECT c FROM Category c WHERE UPPER(c.category) = UPPER(:category)")
+    Optional<Category> findByCategory(@Param("category") String category);
 }
--- src/main/java/se/lexicon/recipeassignmentapi/repository/IngredientRepository.java
+++ src/main/java/se/lexicon/recipeassignmentapi/repository/IngredientRepository.java
@@ -1,7 +1,17 @@
 package se.lexicon.recipeassignmentapi.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import se.lexicon.recipeassignmentapi.model.Ingredient;
 
+import java.util.Optional;
+
 public interface IngredientRepository extends JpaRepository<Ingredient, String> {
+    @Query("SELECT i FROM Ingredient i WHERE UPPER(i.ingredientName) = UPPER(:ingredientName)")
+    Optional<Ingredient> findByIngredientName(@Param("ingredientName") String ingredientName);
+    @Query("SELECT i FROM Ingredient i WHERE UPPER(i.ingredientName) LIKE UPPER(CONCAT(:search,'%'))")
+    Page<Ingredient> searchByIngredientName(@Param("search") String search, Pageable pageable);
 }
