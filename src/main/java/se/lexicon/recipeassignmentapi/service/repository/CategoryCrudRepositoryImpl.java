019e6710b29ca171c44be14a17f3ef2426b7261a
Erik Svensson <eosvensson@gmail.com>
2022-02-18 14:25:21 +0100

    Updated CategoryCrudRepositoryImpl

--- src/main/java/se/lexicon/recipeassignmentapi/service/repository/CategoryCrudRepositoryImpl.java
+++ src/main/java/se/lexicon/recipeassignmentapi/service/repository/CategoryCrudRepositoryImpl.java
@@ -2,29 +2,61 @@ package se.lexicon.recipeassignmentapi.service.repository;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import se.lexicon.recipeassignmentapi.exception.AppResourceNotFoundException;
 import se.lexicon.recipeassignmentapi.model.dto.CategoryDto;
 import se.lexicon.recipeassignmentapi.model.entity.Category;
+import se.lexicon.recipeassignmentapi.repository.CategoryRepository;
+
+import java.util.function.Supplier;
 
 @Service
 @Transactional
 public class CategoryCrudRepositoryImpl implements CategoryCrudRepository{
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryCrudRepositoryImpl(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public Supplier<AppResourceNotFoundException> getException(String message){
+        return () ->  new AppResourceNotFoundException(message);
+    }
+
     @Override
     public Category create(CategoryDto form) {
-        return null;
+        if(form == null) throw new IllegalArgumentException("CategoryDto was null");
+        form.setCategoryName(form.getCategoryName().trim());
+        return categoryRepository.findByCategory(form.getCategoryName())
+                .orElseGet(() -> categoryRepository.save(new Category(null, form.getCategoryName())));
     }
 
     @Override
     public Category findById(String id) {
-        return null;
+        return categoryRepository.findById(id)
+                .orElseThrow(getException("Could not find category with id " + id));
     }
 
     @Override
     public Category update(String id, CategoryDto form) {
-        return null;
+        if(id == null) throw new IllegalArgumentException("Id was null");
+        if(form == null) throw new IllegalArgumentException("CategoryDto was null");
+        if(!id.equals(form.getId())){
+            throw new IllegalStateException("Id didn't match CategoryDto.id");
+        }
+        Category category = findById(id);
+        var optional = categoryRepository.findByCategory(form.getCategoryName().trim());
+        if(optional.isPresent() && !optional.get().getId().equals(id)){
+            throw new IllegalArgumentException("category: " + form.getCategoryName().trim() + " already exists");
+        }
+        category.setCategoryName(form.getCategoryName().trim());
+
+        return categoryRepository.save(category);
     }
 
     @Override
     public boolean delete(String id) {
-        return false;
+        categoryRepository.deleteById(id);
+        return !categoryRepository.existsById(id);
     }
 }
