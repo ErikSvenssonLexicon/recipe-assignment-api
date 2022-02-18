903c18c377bb1f3ae8cc9589cbd63c2cbb1fb4e1
Erik Svensson <eosvensson@gmail.com>
2022-02-18 08:53:43 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/recipeassignmentapi/RecipeAssignmentApiApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.recipeassignmentapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeAssignmentApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeAssignmentApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/recipeassignmentapi/RecipeAssignmentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.recipeassignmentapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeAssignmentApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
