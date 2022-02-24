272c28c61eca844f8901fa3086d81d12dd9dce30
Erik Svensson <eosvensson@gmail.com>
2022-02-24 14:55:49 +0100

    In RecipeIngredientCrudRepository added additional method create that takes a Recipe object in addition to a dto object

--- src/main/java/se/lexicon/recipeassignmentapi/service/repository/RecipeIngredientCrudRepository.java
+++ src/main/java/se/lexicon/recipeassignmentapi/service/repository/RecipeIngredientCrudRepository.java
@@ -1,8 +1,10 @@
 package se.lexicon.recipeassignmentapi.service.repository;
 
 import se.lexicon.recipeassignmentapi.model.dto.RecipeIngredientDto;
+import se.lexicon.recipeassignmentapi.model.entity.Recipe;
 import se.lexicon.recipeassignmentapi.model.entity.RecipeIngredient;
 import se.lexicon.recipeassignmentapi.service.GenericCrudRepository;
 
 public interface RecipeIngredientCrudRepository extends GenericCrudRepository<RecipeIngredient, String, RecipeIngredientDto> {
+    RecipeIngredient create(RecipeIngredientDto form, Recipe recipe);
 }
