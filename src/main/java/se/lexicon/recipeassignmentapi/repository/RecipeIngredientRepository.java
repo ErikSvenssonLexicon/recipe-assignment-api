ebe03f1d3931776354cfbd41a6312c65b8a3a07f
Erik Svensson <eosvensson@gmail.com>
2022-02-24 14:53:25 +0100

    Added countQuery in RecipeIngredientRepository used in automatic cleanup of the database

--- src/main/java/se/lexicon/recipeassignmentapi/repository/RecipeIngredientRepository.java
+++ src/main/java/se/lexicon/recipeassignmentapi/repository/RecipeIngredientRepository.java
@@ -1,7 +1,11 @@
 package se.lexicon.recipeassignmentapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import se.lexicon.recipeassignmentapi.model.entity.RecipeIngredient;
 
 public interface RecipeIngredientRepository extends JpaRepository<RecipeIngredient, String> {
+    @Query("SELECT COUNT(ri.id) FROM RecipeIngredient ri WHERE ri.ingredient.id = :ingredientId")
+    long countUsagesByIngredientId(@Param("ingredientId") String ingredientId);
 }
